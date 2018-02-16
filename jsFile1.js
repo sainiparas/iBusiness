f3ac67d7e27791cab5fd5e230afbf1074f082354
parassaini <31025565+sainiparas@users.noreply.github.com>
2018-02-16 15:32:16 -0200

    Add files via upload

---
+++ jsFile1.js
@@ -0,0 +1,23 @@
+function abc()
+{
+	var ab = new Date();
+	var cd = ab.toLocaleDateString();
+	document.getElementById("btn1").innerHTML =cd + "(m-d-y)";
+	var k = window.setInterval(def,1000);
+	
+}
+
+
+function def()
+{
+	var a = new Date();
+	var b = a.toLocaleTimeString();
+	document.getElementById("btn2").innerHTML = b;
+
+}
+
+
+function markUp()
+{
+    document.getElementsByClassName("txt-area").css("fontWeight","bold");
+}
